2a008b017002d7f6126d806ae87dd7f70e64e92c
Kousuke Nagatsuka <koron393@gmail.com>
2016-03-04 17:44:56 +0900

    hammerheadcaf: Add optimizations and to show enabled optis in about phone.

--- cm.mk
+++ cm.mk
@@ -23,3 +23,17 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=hammerhead \
     BUILD_FINGERPRINT=google/hammerhead/hammerhead:5.1.1/LMY48M/2167285:user/release-keys \
     PRIVATE_BUILD_DESC="hammerhead-user 5.1.1 LMY48M 2167285 release-keys"
+
+# RR Opti
+TARGET_ARCH := arm
+TARGET_CPU_VARIANT := krait
+TARGET_TOOLS_PREFIX=prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9-cortex-a15/bin/arm-linux-androideabi-
+TARGET_TOOLCHAIN_ROOT := prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9-cortex-a15
+TARGET_TC_ROM := 4.9-cortex-a15
+TARGET_TC_KERNEL := 4.9-cortex-a15
+RR_STRICT := true
+RR_GRAPHITE := true
+RR_KRAIT := true
+RROPTI := true
+RR_PIPE := true
+RR_O3 := true
